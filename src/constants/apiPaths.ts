b398fe8de0d05af5d763389bf09cbfa3f2acb076
vishalv <connectwithvishalv@gmail.com>
2022-08-01 16:49:27 +0530

    add import url to apipaths

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@
 const API_PATHS = {
   product: 'https://ywjkmsxky5.execute-api.ap-south-1.amazonaws.com/dev/products',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://d6qi6x3r91.execute-api.ap-south-1.amazonaws.com/dev/import',
   bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 };
